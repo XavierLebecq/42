ee284acbae435eba0ac0088f353b6f1fc66954cc
xlebecq <xlebecq@student.42.fr>
2023-11-17 07:42:08 +0100

    Fri Nov 17 07:42:08 AM CET 2023

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,19 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xlebecq <xlebecq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/12 18:44:29 by xlebecq           #+#    #+#             */
-/*   Updated: 2023/11/12 21:14:48 by xlebecq          ###   ########.fr       */
+/*   Created: 2023/11/16 15:40:28 by xlebecq           #+#    #+#             */
+/*   Updated: 2023/11/16 17:22:08 by xlebecq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(voi
+#include "libft.h"
 
-
-esiwdjwekfjk;lfflkfjkl;:wd
-
-
-test
+void *ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+	
+	if (count * size / size != count)
+		return (NULL);
+	if (count == 0 || size == 0)
+		return ((void *)malloc(1));
+	ptr = (void*)malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
---
+++ ft_lstdelone.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xlebecq <xlebecq@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 10:48:15 by xlebecq           #+#    #+#             */
+/*   Updated: 2023/11/16 10:50:37 by xlebecq          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (del != NULL && alst != NULL)
+	{
+		del((**alst).content, (**alst).content_size);
+		free(*alst);
+		*alst = NULL;
+	}
+}
