414563659f4221a51846490c1e31c62a0c769ad3
root <root@Xavier>
2023-11-27 05:07:36 +0100

    Mon Nov 27 05:07:36 CET 2023

--- Cursus/libft/libft.h
+++ Cursus/libft/libft.h
@@ -36,7 +36,7 @@ void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
-void				*ft_memset(void *b, int c, size_t len);
+//void				*ft_memset(void *b, int c, size_t len);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
