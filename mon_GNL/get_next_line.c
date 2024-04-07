83308d19e0a5df3d88d3fb1065b22070202ba172
xlebecq <xlebecq@student.42.fr>
2024-04-07 16:16:53 +0200

    dim. 07 avril 2024 16:16:53 CEST

--- mon_GNL/get_next_line.c
+++ mon_GNL/get_next_line.c
@@ -6,12 +6,38 @@
 /*   By: xlebecq <xlebecq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 10:04:24 by xlebecq           #+#    #+#             */
-/*   Updated: 2024/04/05 11:06:34 by xlebecq          ###   ########.fr       */
+/*   Updated: 2024/04/07 16:16:21 by xlebecq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	main(int argc, char **argv)
+void	get_next_line(int fd)
 {
+        char    *str;
 
+	str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+
+        read(fd, str, BUFFER_SIZE);
+		printf ("str = %s\nFIN", str);
+
+	}
+
+	int	main(int argc, char **argv)
+	{
+	int	fd;
+
+	if (argc < 2)
+	{
+		printf("usage: %s <file>\n", argv[0]);
+		return (1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		return (1);
+	}
+	get_next_line(fd);
+	return 0;
+}
--- mon_GNL/get_next_line.h
+++ mon_GNL/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: krakren <krakren@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 04:33:06 by xlebecq           #+#    #+#             */
-/*   Updated: 2024/04/04 23:28:28 by krakren          ###   ########.fr       */
+/*   Updated: 2024/04/07 09:53:50 by xlebecq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 # include <fcntl.h>
 # include <stdio.h>
-# include <stdlib.h>
 # include <unistd.h>
+# include <stdlib.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 5
@@ -24,20 +24,10 @@
 
 typedef struct s_list
 {
-	char			content[BUFFER_SIZE + 1];
-	char			reste[BUFFER_SIZE + 1];
+	char		content;
 	struct s_list	*next;
 }					t_list;
 
-char	*get_next_line(int fd);
-t_list	*ft_lstnew(char *content);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlen(const char *s);
-char    *ft_strchr(const char *s, int c);
-int		ft_strchr_index(const char *s, int c);
-void    ft_lstclear(t_list **lst);
-
-
+void	get_next_line(int fd);
 
 #endif
