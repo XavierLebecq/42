5b0ac4ebff70db8d81380f05c952930b782455c6
xlebecq <xlebecq@student.42.fr>
2024-04-15 23:03:53 +0200

    lun. 15 avril 2024 23:03:53 CEST

--- mon_GNL/get_next_line_bonus.c
+++ mon_GNL/get_next_line_bonus.c
@@ -6,18 +6,58 @@
 /*   By: xlebecq <xlebecq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 12:23:44 by xlebecq           #+#    #+#             */
-/*   Updated: 2024/04/14 17:04:11 by xlebecq          ###   ########.fr       */
+/*   Updated: 2024/04/15 23:03:29 by xlebecq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-void	ft_create_list(t_list **lst, char *buffer)
+void    ft_read(t_list **lst, int fd)
+{
+    int     read_bytes;
+    char    *buffer;
+
+    while(!ft_findnl(*lst))
+    {
+        buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+        if (!buffer) 
+            return ; 
+        read_bytes = read (fd, buffer, BUFFER_SIZE);
+        if (!read_bytes)
+        {   
+            free(buffer);
+            return ;
+        }
+        buffer[read_bytes] = '\0';
+        ft_create_lst(lst, buffer);
+    }
+}
+
+int	ft_findnl(t_list *lst)
+{
+	int	i;
+
+	if (!lst)
+		return (0);
+	while (lst)
+	{
+		i = 0;
+		while (lst->content[i] != '\0')
+		{
+			if (lst->content[i] == '\n')
+				return (1);
+			i = i + 1;
+		}
+		lst = lst->next;
+	}
+}
+
+void	ft_create_lst(t_list **lst, char *buffer)
 {
 	t_list	*new;
 	t_list	*last;
 
-	new = malloc(t_list *)malloc(sizeof(t_list));
+	new = (t_list *)malloc(sizeof(t_list));
 	if (!new)	
 	{
 		free(buffer);
@@ -31,54 +71,111 @@ void	ft_create_list(t_list **lst, char *buffer)
 	{
 		last = ft_lstlast(*lst);
 		last->next = new;
-	}	
-}
-
-
-t_list	*ft_lstlast(t_list *lst)
-{
-	if (!lst)
-		return (NULL);
-	while (lst->next != NULL)
-		lst = lst->next;
-	return (lst);
-}
-
-void	ft_read(t_list **lst, int fd)
-{
-	int		read_bytes;
-	char	*buffer;
-
-	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return ;
-	read_bytes = read (fd, buffer, BUFFER_SIZE);
-	if (!read_bytes)
-	{
-		free(buffer);
-		return ;
+		
 	}
-	buffer[read_bytes] = '\0';
-	printf ("buffer= %s\n", buffer);
-	ft_create_lst(lst, buffer)
 }
+
+t_list  *ft_lstlast(t_list *lst)
+{
+    if (!lst)
+        return (NULL);
+    while (lst->next != NULL)
+        lst = lst->next;
+    return (lst);
+}
+
+char    *ft_concatenate_lst(t_list *lst)
+{
+    int len;
+    char    *line;
+
+    if (!lst)
+        return (NULL);
+    len = ft_lstsize_nl(lst);
+    line = (char *)malloc(len + 1);
+    if (!line)
+        return (NULL);
+    ft_create_line(lst, line);
+    return (line);
+}
+
+int	ft_lstsize_nl(t_list *lst)
+{
+	int	i;
+	int	j;
+	
+	j = 0;
+	while (lst)
+	{
+		i = 0;
+		while (lst->content[i] != '\0')
+		{
+			if (lst->content[i] == '\n')
+			{
+		    	j++;
+				return (j);
+			}
+			i++;
+			j++;
+		}
+		lst = lst->next;
+	}
+	return (j);
+}
+
+void	ft_create_line(t_list *lst, char *line)
+{
+	int	i;
+	int j;
+
+	j = 0;
+	while (lst)
+	{
+		i = 0;	
+		while(lst->content[i] != '\0')
+		{
+			if (lst->content[i] == '\n')
+			{
+				line[j] = '\n';
+				line[j + 1] = '\0';
+				return ;
+			}
+			line[j] = lst->content[i];
+			i++;
+			j++;
+		}
+		lst = lst->next;
+	}
+	line[i] = '\0';
+}		
 
 char	*get_next_line(int fd)
 {
 	static t_list	*lst = NULL;
+	char			*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read (fd, NULL, 0) < 0)
 		return (NULL);
 	ft_read(&lst, fd);
-	return (NULL);
+	if (!lst)
+		return (NULL);
+	line = ft_concatenate_lst(lst);
+	return (line);
 }
 
 int	main(int argc, char **argv)
 {
 	int	fd;
+	char	*line;
 
 	fd = open (argv[1], O_RDONLY);
-	printf("GNL= %s\n", get_next_line(fd));
+	if (fd == -1)
+	{
+		printf("erreur open");
+		return (1);
+	}
+	while ((line = get_next_line(fd)) != NULL)
+		printf("GNL = %s", line);
 	close (fd);
 	return (0);
 }
--- mon_GNL/get_next_line_bonus.h
+++ mon_GNL/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: xlebecq <xlebecq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 13:30:58 by xlebecq           #+#    #+#             */
-/*   Updated: 2024/04/14 16:23:57 by xlebecq          ###   ########.fr       */
+/*   Updated: 2024/04/15 22:46:17 by xlebecq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,14 @@ typedef struct s_list
 	char			*content;
 	struct s_list	*next;
 }					t_list;
+
+void    ft_create_lst(t_list **lst, char *buffer);
+void    ft_read(t_list **lst, int fd);
+int ft_lstsize_nl(t_list *lst);
+char    *ft_concatenate_lst(t_list *lst);
+void    ft_create_line(t_list *lst, char *line);
+char    *get_next_line(int fd);
+int	ft_findnl(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
