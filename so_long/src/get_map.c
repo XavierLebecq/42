3dfea8fd2136b339150842536ced45174b2df6fa
xlebecq <xlebecq@student.42.fr>
2024-06-03 18:53:50 +0200

    Mon Jun  3 06:53:50 PM CEST 2024

---
+++ so_long/src/get_map.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_map.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xlebecq <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 18:23:29 by xlebecq           #+#    #+#             */
+/*   Updated: 2024/06/03 18:42:57 by xlebecq          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+size_t  ft_count_lines(t_var *game)
+{
+    size_t count = 0;
+    game->line = get_next_line(game->fd);
+    while (game->line)
+    {
+        free(game->line);
+        game->line = get_next_line(game->fd);
+        count++;
+    }
+    close (game->fd);
+    ft_printf("------>%d\n", count);
+    return count;
+}
+
+char    **read_map(t_var *game)
+{
+    size_t i = 0;
+
+    game->map = (char **)malloc(sizeof(char *) * (game->map_lines + 1));
+    if (!game->map)
+        return (NULL);
+    game->line = get_next_line(game->fd);
+    while (game->line != NULL)
+    {
+        game->map[i] = game->line;
+        game->line = get_next_line(game->fd);
+        i++;
+    }
+    game->map[i] = NULL;
+
+    close(game->fd);
+    return (game->map);
+}
+
+void print_map(t_var *game)
+{
+    if (!game->map)
+    {
+        ft_printf("Error: Map is not loaded.\n");
+        return;
+    }
+
+    ft_printf("Map content:\n");
+    for (int i = 0; i < game->map_lines; i++)
+    {
+        ft_printf("%s", game->map[i]);
+    }
+}
+
+
--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -6,54 +6,20 @@
 /*   By: xlebecq <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:44:20 by xlebecq           #+#    #+#             */
-/*   Updated: 2024/06/03 10:04:50 by xlebecq          ###   ########.fr       */
+/*   Updated: 2024/06/03 18:43:09 by xlebecq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void print_map(t_var *game)
-{
-    if (!game->map)
-    {
-        ft_printf("Error: Map is not loaded.\n");
-        return;
-    }
-
-    ft_printf("Map content:\n");
-    for (int i = 0; i < game->map_lines; i++)
-    {
-        ft_printf("%s", game->map[i]);
-    }
-}
-
-size_t	count_lines(t_var *game)
-{
-	char	*line;
-	size_t count = 0;
-	line = get_next_line(game->fd);
-	while (line)
-	{
-		free(line);
-		line = get_next_line(game->fd);
-		count++;
-	}
-	close (game->fd);
-	ft_printf("%d", count);
-	return count;
-}
-
 int	validate_map(t_var *game)
 {
-	int exit_count;
-	int	player_count;
-	int	collectible_count = 0;
 	int i;
 	int j;
 
-	exit_count = 0;
-	player_count = 0;
-	collectible_count = 0;
+	game->exit_count = 0;
+	game->player_count = 0;
+	game->collectible_count = 0;
 	i = 0;
 
 	while (i < game->map_lines)
@@ -62,94 +28,44 @@ int	validate_map(t_var *game)
 		while (game->map[i][j] != '\0')
 		{
 			if (game->map[i][j] == 'E')
-				exit_count++;
+				game->exit_count++;
 			else if (game->map[i][j] == 'P')
-				player_count++;
+				game->player_count++;
 			else if (game->map[i][j] == 'C')
-				collectible_count++;
+				game->collectible_count++;
 			j++;
 		}
 		i++;
 	}
-	if (exit_count != 1 || player_count != 1 || collectible_count < 1)
+	if (game->exit_count != 1 || game->player_count != 1 || game->collectible_count < 1)
 	{
-		if (exit_count != 1)
+		if (game->exit_count != 1)
+		{
 			ft_printf("Error: Map must contain exactly one exit.\n");
-		if (player_count != 1)
+			exit (EXIT_FAILURE);
+		}
+		if (game->player_count != 1)
+		{
 			ft_printf("Error: Map must contain exactly one player.\n");
-		if (collectible_count < 1)
+			exit (EXIT_FAILURE);
+		}
+		if (game->collectible_count < 1)
+		{
 			ft_printf("Error: Map must contain at least one collectible.\n");
+			exit (EXIT_FAILURE);
+		}
 		return (0);
 	}
 	return (1);
 }
 
-char	**read_map(t_var *game)
-{
-	char **map;
-	char	*line;
-	size_t i = 0;
-
-	map = (char **)malloc(sizeof(char *) * (game->map_lines + 1));
-	if (!map)
-		return (NULL);
-
-	while ((line = get_next_line(game->fd)) != NULL)
-	{
-		map[i] = line;
-		i++;
-	}
-	map[i] = NULL;
-
-	close(game->fd);
-	return (map);
-}
 
 int main(int argc, char **argv)
 {
-	
 	t_var game;
-
 	
-	if (argc != 2)
-	{
-		ft_printf ("Error: invalid number of arguments. Usage: ./so_long <map_file.ber>\n");
-		exit (EXIT_FAILURE);
-	}
-	if (ft_strncmp(&argv[1][ft_strlen(argv[1])-4], ".ber", 4) != 0)
-	{
-		ft_printf ("Error: invalid map file extension. Expected a '.ber' file.\n");
-		exit (EXIT_FAILURE);
-	}
-
-	game.fd = open(argv[1], O_RDONLY);
-	if (game.fd == -1)
-	{
-		perror("Error opening file");
-		exit (EXIT_FAILURE);
-	}
-
-	game.map_lines = count_lines(&game);
-	if (game.map_lines == 0)
-	{
-		ft_printf("Error: The map file is empty or could not be read.\n");
-		close(game.fd);
-		exit(EXIT_FAILURE);
-	}
-
-	game.fd = open(argv[1], O_RDONLY);
-	game.map = read_map(&game);
-	if (!game.map)
-	{
-		ft_printf("Error: Failed to read the map.\n");
-		close (game.fd);
-		exit(EXIT_FAILURE);
-	}
-	print_map(&game);
-
-
-
-
+	ft_check_args(argc, argv, &game);
+	validate_map(&game);
 
 	void *img;
 	int img_width;
