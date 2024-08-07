6017f769883d758319c6d0f068eb997517b4a351
xlebecq <xlebecq@student.42.fr>
2024-08-07 20:30:29 +0200

    test

--- Cursus/get_next_line/text.c
+++
@@ -1,4 +0,0 @@
-
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s --track-fds=yes ./so_long map.ber
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s --track-fds=yes ./so_long map.ber
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s --track-fds=yes ./so_long map.ber
